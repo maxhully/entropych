87b2526183781b99059a4f72c0974fcfc94c4594
Max Hully <max.hully@gmail.com>
2025-04-08 17:40:31 -0400

    More stuff!

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/maxhully/messing_with_go
 
 go 1.22.6
+
+require crawshaw.io/sqlite v0.3.2 // indirect
--- main.go
+++ main.go
@@ -1,14 +1,23 @@
 package main
 
 import (
+	"context"
 	"fmt"
 	"html/template"
 	"log"
 	"net/http"
+
+	"crawshaw.io/sqlite"
+	"crawshaw.io/sqlite/sqlitex"
 )
 
 type User struct {
+	Id   int
 	Name string
+}
+
+func (u *User) Exists() bool {
+	return u.Id != -1
 }
 
 func helloWorld(w http.ResponseWriter, r *http.Request) {
@@ -17,11 +26,34 @@ func helloWorld(w http.ResponseWriter, r *http.Request) {
 
 type App struct {
 	templates *template.Template
+	dbpool    *sqlitex.Pool
 }
 
 func NewApp() *App {
+	dbpool, err := sqlitex.Open("test.db", 0, 10)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	conn := dbpool.Get(context.TODO())
+	if conn == nil {
+		log.Fatal("I couldn't get a connection!")
+	}
+	defer dbpool.Put(conn)
+
+	err = sqlitex.ExecScript(conn, `
+		create table if not exists user (
+			user_id integer primary key not null,
+			name text not null
+		);
+	`)
+	if err != nil {
+		log.Fatalf("couldn't set up db: %s", err)
+	}
+
 	return &App{
 		templates: template.Must(template.ParseGlob("templates/*")),
+		dbpool:    dbpool,
 	}
 }
 
@@ -31,10 +63,54 @@ func (app *App) helloUser(w http.ResponseWriter, r *http.Request) {
 	if name == "" {
 		name = "Max"
 	}
-	u := User{Name: name}
-	err := app.templates.ExecuteTemplate(w, "hello.html", u)
+
+	conn := app.dbpool.Get(r.Context())
+	defer app.dbpool.Put(conn)
+
+	var user *User
+	user = nil
+
+	sqlitex.Exec(conn, "select user_id, name from user where name = ? limit 1", func(stmt *sqlite.Stmt) error {
+		user = &User{
+			Id:   stmt.ColumnInt(0),
+			Name: stmt.ColumnText(1),
+		}
+		return nil
+	}, name)
+
+	if user == nil {
+		user = &User{
+			Id:   -1,
+			Name: name,
+		}
+	}
+	err := app.templates.ExecuteTemplate(w, "hello.html", user)
 	if err != nil {
 		log.Fatalf("executing template: %s", err)
+	}
+}
+
+func (app *App) createUser(w http.ResponseWriter, r *http.Request) {
+	if r.Method == "POST" {
+		r.ParseForm()
+		name := r.PostForm.Get("name")
+
+		conn := app.dbpool.Get(r.Context())
+		defer app.dbpool.Put(conn)
+
+		err := sqlitex.Exec(conn, "insert into user (name) values (?);", nil, name)
+		if err != nil {
+			log.Printf("error: %s", err)
+			http.Error(w, "500 Internal Server Error", http.StatusInternalServerError)
+			return
+		}
+
+		http.Redirect(w, r, "/", http.StatusSeeOther)
+	} else {
+		err := app.templates.ExecuteTemplate(w, "create.html", nil)
+		if err != nil {
+			log.Fatalf("executing template: %s", err)
+		}
 	}
 }
 
@@ -44,6 +120,7 @@ func main() {
 	mux := http.NewServeMux()
 	mux.HandleFunc("/", helloWorld)
 	mux.HandleFunc("/user", app.helloUser)
+	mux.HandleFunc("/users/new", app.createUser)
 	mux.Handle("/static/", http.StripPrefix("/static", http.FileServer(http.Dir("./static"))))
 	http.ListenAndServe(":7777", mux)
 }
