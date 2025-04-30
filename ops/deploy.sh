043f6f0e4b7c8103606a458881a29d294ade9fd8
Max Hully <max.hully@gmail.com>
2025-04-30 09:53:11 -0400

    Deploy script tweak

--- ops/deploy.sh
+++ ops/deploy.sh
@@ -12,6 +12,7 @@ mkdir -p ./build/
 go test ./...
 
 # TODO: maybe I should just push the files to the server and compile it there
+docker container rm entropych_build || true
 docker build --platform=linux/amd64 -f ops/entropych.Dockerfile -t entropych_build .
 docker container create --platform=linux/amd64 --name=entropych_build entropych_build
 docker container cp entropych_build:/go/src/entropych/server ./build/server
