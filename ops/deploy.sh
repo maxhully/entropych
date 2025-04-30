72a2f4ddb999470ee6e65561ac6e4de2ab2364b6
Max Hully <max.hully@gmail.com>
2025-04-29 23:04:59 -0400

    Fix rsync in deploy

--- ops/deploy.sh
+++ ops/deploy.sh
@@ -11,6 +11,7 @@ mkdir -p ./build/
 
 go test ./...
 
+# TODO: maybe I should just push the files to the server and compile it there
 docker build --platform=linux/amd64 -f ops/entropych.Dockerfile -t entropych_build .
 docker container create --platform=linux/amd64 --name=entropych_build entropych_build
 docker container cp entropych_build:/go/src/entropych/server ./build/server
@@ -25,11 +26,12 @@ EOL
 remote_binary_version="/home/entropych/versions/server-$(TZ=UTC date -u +"%s")-$(git rev-parse --short HEAD)"
 
 # TODO: only do this if it changed, so systemd doesn't ask for a daemon-reload
-scp ops/entropych.service "$server_ssh:/etc/systemd/system/entropych.service"
-scp ops/entropych.env "$server_ssh:/etc/entropych/entropych.env"
+# scp ops/entropych.service "$server_ssh:/etc/systemd/system/entropych.service"
+# scp ops/entropych.env "$server_ssh:/etc/entropych/entropych.env"
 
 scp build/server "$server_ssh:$remote_binary_version"
-rsync --exclude=".*\.DS_Store" ./static/ "$server_ssh:/home/entropych/static"
+# TODO: embed this in the binary, so that it's deployed as part of the single step below
+rsync -rv --progress --exclude=".*\.DS_Store" ./static/ "$server_ssh:/home/entropych/static"
 # shellcheck disable=SC2087
 ssh -q -T "$server_ssh" <<EOL
 	nohup sh -c "\
